d1f310f41df7d8f0e9103fe58b1b21040525b278
martinafilice <fmarty2308@gmail.com>
2024-03-28 21:51:15 +0100

    Adding the Add and Remove Announcement buttons to the bulletin board and creating the AddAnnouncementActivity page to enter the information for an announcement

--- app/src/main/java/com/social/insubriasocial/AddAnnouncementActivity.kt
+++ app/src/main/java/com/social/insubriasocial/AddAnnouncementActivity.kt
@@ -1,28 +1,22 @@
 package com.social.insubriasocial
 
-import android.content.Intent
+import android.annotation.SuppressLint
 import android.os.Bundle
-import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class Bacheca : AppCompatActivity() {
+class AddAnnouncementActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_bacheca)
+        setContentView(R.layout.activity_add_announcement)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
-        }
-
-        val buttonAggiuntaAnnuncio = findViewById<Button>(R.id.buttonAggiuntaAnnuncio)
-        buttonAggiuntaAnnuncio.setOnClickListener{
-            val intent = Intent(this, AddAnnouncementActivity::class.java)
-            startActivity(intent)
         }
     }
 }
