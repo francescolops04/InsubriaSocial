cdf0760fe20d45958303dbe10740a2c5229f68ca
martinafilice <fmarty2308@gmail.com>
2024-03-31 15:26:03 +0200

    Declaration of variable "systemBars" before its use and initialized in the onCreate function, in the AddAnnouncementActivity page

--- app/src/main/java/com/social/insubriasocial/AddAnnouncementActivity.kt
+++ app/src/main/java/com/social/insubriasocial/AddAnnouncementActivity.kt
@@ -8,14 +8,20 @@ import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class AddAnnouncementActivity : AppCompatActivity() {
+
+    private lateinit var systemBars: WindowInsetsCompat
+
+    init {
+        systemBars = WindowInsetsCompat.Builder().build()
+    }
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_add_announcement)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            systemBars = insets
+            v.setPadding(systemBars.systemWindowInsetLeft, systemBars.systemWindowInsetTop, systemBars.systemWindowInsetRight, systemBars.systemWindowInsetBottom)
             insets
         }
     }
