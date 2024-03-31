fbfa265b1bd3feff69d3e6c3bf1a0b0f21b62463
martinafilice <fmarty2308@gmail.com>
2024-03-31 15:46:42 +0200

    Modified the measurements of the confirmation button and editTexts in the activity_add_annoincement.xml and removed the "Back" button in the bulletin board

--- app/src/main/java/com/social/insubriasocial/Bacheca.kt
+++ app/src/main/java/com/social/insubriasocial/Bacheca.kt
@@ -1,11 +1,9 @@
 package com.social.insubriasocial
 
 import android.content.Intent
-import android.graphics.Insets
 import android.os.Bundle
 import android.widget.Button
 import android.widget.Toast
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
