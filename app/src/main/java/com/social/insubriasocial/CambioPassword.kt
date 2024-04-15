91c7a588b82df488f98e230161b243d9d3afb3f2
martinafilice <fmarty2308@gmail.com>
2024-04-15 13:54:11 +0200

    Added old password check

--- app/src/main/java/com/social/insubriasocial/CambioPassword.kt
+++ app/src/main/java/com/social/insubriasocial/CambioPassword.kt
@@ -4,7 +4,9 @@ import android.annotation.SuppressLint
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 
 lateinit var btnBackCP: Button
 lateinit var btnConfirmCP: Button
@@ -23,5 +25,34 @@ class CambioPassword : AppCompatActivity() {
         oldpassword = findViewById<EditText>(R.id.PasswordAttualeCP)
         passwordChanged = findViewById<EditText>(R.id.NuovaPasswordCP)
         passwordChangedConfirm = findViewById<EditText>(R.id.ConfermaPasswordCP)
+
+        btnConfirmCP.setOnClickListener{
+            confrontaPasswordFirebase(oldpassword){ CorrectPassword ->
+                if(CorrectPassword){
+                    Toast.makeText(this, "giusto", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "La password è sbagliata", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+    }
+
+    fun confrontaPasswordFirebase(passwordEditText: EditText, onComplete: (Boolean) -> Unit) {
+        val auth = FirebaseAuth.getInstance()
+
+        val email = auth.currentUser?.email
+
+        val passwordInserita = passwordEditText.text.toString()
+
+        if (email != null && passwordInserita.isNotEmpty()) {
+            auth.signInWithEmailAndPassword(email, passwordInserita)
+                .addOnCompleteListener { task ->
+                    if (task.isSuccessful) {
+                        onComplete(true)
+                    } else {
+                        onComplete(false)
+                    }
+                }
+        }
     }
 }
