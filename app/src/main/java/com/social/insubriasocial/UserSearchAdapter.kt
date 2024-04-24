f6cb9125ef96e026473b3477d44d36a30d3bad7b
martinafilice <fmarty2308@gmail.com>
2024-04-24 14:19:09 +0200

    Improving Custom List Search

--- app/src/main/java/com/social/insubriasocial/UserSearchAdapter.kt
+++ app/src/main/java/com/social/insubriasocial/UserSearchAdapter.kt
@@ -7,24 +7,24 @@ import android.view.ViewGroup
 import android.widget.ArrayAdapter
 import android.widget.TextView
 
-class UserSearchAdapter(context: Context, data: ArrayList<String>) : ArrayAdapter<String>(context, R.layout.custom_list, data) {
+class UserSearchAdapter(context: Context, data: ArrayList<String>) : ArrayAdapter<String>(context, R.layout.custom_list_search, data) {
 
     override fun getView(position: Int, convertView: View?, parent: ViewGroup): View {
         var convertView = convertView
         if (convertView == null) {
-            convertView = LayoutInflater.from(context).inflate(R.layout.custom_list, parent, false)
+            convertView = LayoutInflater.from(context).inflate(R.layout.custom_list_search, parent, false)
         }
 
         val item = getItem(position)
 
-        val textViewUser = convertView!!.findViewById<TextView>(R.id.textViewUser)
-        val textViewTitle = convertView.findViewById<TextView>(R.id.textViewTitle)
+        val textViewSearchUser = convertView!!.findViewById<TextView>(R.id.textViewSearchUser)
+        val textViewSearchName = convertView.findViewById<TextView>(R.id.textViewSearchName)
 
         val parts = item?.split("\n")
         if (parts != null) {
             if (parts.size >= 2) {
-                textViewUser.text = parts[0]
-                textViewTitle.text = parts[1]
+                textViewSearchUser.text = parts[0]
+                textViewSearchName.text = parts[1]
             }
         }
 
