dee4987009968c7dc3a4d59631881a149f71dad8
PietroColombo89 <lopsfrancesco3@gmail.com>
2024-04-28 11:54:28 +0200

    Adding faculty filters

--- app/src/main/java/com/social/insubriasocial/SistemaDiRicerca.kt
+++ app/src/main/java/com/social/insubriasocial/SistemaDiRicerca.kt
@@ -3,10 +3,13 @@ package com.social.insubriasocial
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.ListView
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doAfterTextChanged
 import com.google.firebase.firestore.FirebaseFirestore
@@ -20,6 +23,7 @@ lateinit var btnSettingsSe: ImageView
 lateinit var adapterSearch: ArrayAdapter<String>
 lateinit var searchList: ListView
 lateinit var searchText: EditText
+lateinit var spinnerFilter: Spinner
 
 class SistemaDiRicerca : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -34,15 +38,32 @@ class SistemaDiRicerca : AppCompatActivity() {
         btnSettingsSe = findViewById<ImageView>(R.id.SettingsSe)
         searchList = findViewById<ListView>(R.id.list_user)
         searchText = findViewById<EditText>(R.id.searchUser)
+        spinnerFilter = findViewById<Spinner>(R.id.spinnerSR)
 
 
         searchList = findViewById<ListView>(R.id.list_user)
         adapterSearch = UserSearchAdapter(this, ArrayList())
         searchList.adapter = adapterSearch
 
-        searchText.doAfterTextChanged { editable ->
-            searchUser(editable.toString())
+        spinnerFilter.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onItemSelected(parent: AdapterView<*>, view: View?, position: Int, id: Long) {
+                val selectedItem = parent.getItemAtPosition(position).toString()
+                if (!selectedItem.equals("Seleziona la facoltà")) {
+                    searchText.doAfterTextChanged { editable ->
+                        searchUserFilter(editable.toString(), selectedItem)
+                    }
+                } else {
+                    searchText.doAfterTextChanged { editable ->
+                        searchUser(editable.toString())
+                    }
+                }
+            }
+
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
         }
+
 
         btnSettingsSe.setOnClickListener {
             val intent = Intent(this, Settings::class.java)
@@ -77,6 +98,31 @@ class SistemaDiRicerca : AppCompatActivity() {
                     val username = document.getString("username")
                     if (nome != null && cognome != null && username != null) {
                         if (username.contains(user.trim()) || nome.contains(user.trim()) || cognome.contains(user.trim())) {
+                            val searchitemlist = "$username\n$nome $cognome"
+                            searchList.add(searchitemlist)
+                        }
+                    }
+                }
+                adapterSearch.clear()
+                adapterSearch.addAll(searchList)
+            }
+    }
+
+    private fun searchUserFilter(user: String, filter: String) {
+        val db = FirebaseFirestore.getInstance()
+
+
+        db.collection("utenti")
+            .get()
+            .addOnSuccessListener { result ->
+                val searchList = ArrayList<String>()
+                for (document in result) {
+                    val nome = document.getString("nome")
+                    val cognome = document.getString("cognome")
+                    val username = document.getString("username")
+                    val facoltà = document.getString("facoltà")
+                    if (nome != null && cognome != null && username != null && facoltà != null) {
+                        if ((username.contains(user.trim()) || nome.contains(user.trim()) || cognome.contains(user.trim())) && filter == facoltà) {
                             val searchitemlist = "$username\n$nome $cognome"
                             searchList.add(searchitemlist)
                         }
